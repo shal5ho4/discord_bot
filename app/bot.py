597e70d97dfd21da7bfd113397f49637cd029eb7
shal5ho4 <rcxsoor@gmail.com>
2025-09-26 18:15:25 +0900

    added voice chat join record

--- app/bot.py
+++ app/bot.py
@@ -1,14 +1,17 @@
-import inspect
-from datetime import datetime, timezone, timedelta
-
 import discord
-from discord import app_commands
+import inspect
+import json
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+from datetime import datetime, timezone, timedelta
 from discord.ext import commands
+from pathlib import Path
 
 from const import *
-from modal import RegisterView
+# from modal import RegisterView
 # from server import server_thread
 
+JST = timezone(timedelta(hours=+9), 'JST')
 
 intents = discord.Intents.default()
 intents.voice_states = True
@@ -18,7 +21,6 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 tree = bot.tree
-
 
 ##### bot commands #####
 @tree.command(name='henlo', description='Say hello')
@@ -51,7 +53,7 @@ async def list_no_role_members(interaction: discord.Interaction):
 
 
 @tree.command(name='role-member-list', description='ロールごとのメンバーを教えてくれます。')
-@app_commands.describe(role='ロールを選択！')
+@discord.app_commands.describe(role='ロールを選択！')
 async def list_role_members(
     interaction: discord.Interaction,
     role: discord.Role
@@ -84,17 +86,83 @@ async def list_role_members(
 
 
 ##### bot event functions #####
-@bot.event
-async def on_ready():
-    await tree.sync()
-    print(f'Logged in as {bot.user}')
-    # channel = bot.get_channel(CHANNEL_ID)
-    # await channel.send('Voice chat notification READY.')
+
+JOIN_RECORD = Path('join_record.json')  # {"member_id": "timestamp" | null}
+record_start_at = ''
+
+def load_join_record() -> dict:
+    if JOIN_RECORD.exists():
+        with open(JOIN_RECORD, 'r', encoding='utf-8') as f:
+            return json.load(f)
+    return {}
 
 
-# 終了メッセージ用 {channel_id: message_id}
-# TODO: クラスにする？
-active_voice_channels = {}
+def update_join_record(member_id: int, date_null=False):
+    join_record = load_join_record()
+
+    if date_null:
+        date = None
+    else:
+        date = datetime.now(JST).strftime('%Y-%m-%d')
+
+    join_record[str(member_id)] = date
+    print(join_record)
+
+    with open(JOIN_RECORD, 'w', encoding='utf-8') as f:
+        json.dump(join_record, f, ensure_ascii=False, indent=2)
+    print('join record saved.')
+
+
+def remove_join_record(member_id: int):
+    join_record = load_join_record()
+    join_record.pop(str(member_id))
+
+    with open(JOIN_RECORD, 'w', encoding='utf-8') as f:
+        json.dump(join_record, f, ensure_ascii=False, indent=2)
+    print(f'member_id "{member_id}" removed.')
+
+
+def get_inactive_members() -> list[tuple[int, str]]:
+    join_record = load_join_record()
+    inactive_members = []
+
+    for member_id, timestamp in join_record.items():
+        if timestamp:
+            joined_date = datetime.strptime(timestamp, '%Y-%m-%d')
+            delta = datetime.now(JST) - joined_date
+            # if delta.days >= 3:
+            days_ago = f'{delta.days}日前 ({timestamp})'
+        else:
+            days_ago = 'N/A'
+        inactive_members.append((int(member_id), days_ago))
+    
+    return inactive_members
+
+
+scheduler = AsyncIOScheduler()
+
+@scheduler.scheduled_job(CronTrigger(minute=30, timezone='Asia/Tokyo'))
+async def join_record_reminder():
+    """
+    send join record(weekly)
+    """
+    global record_start_at
+
+    inactive_members = get_inactive_members()
+    list_str = ''
+    for t in inactive_members:
+        member_id, days_ago = t
+        list_str += f'<@{member_id}>: {days_ago}\n'
+
+    message = f'record_start_at: {record_start_at}\nget_inactive_members():\n{list_str}'
+
+    channel_id = CHANNEL_ID_TEST_TX if DEBUG else CHANNEL_ID_MANAGE_2
+    channel = bot.get_channel(channel_id)
+    if channel:
+        await channel.send(message, silent=True)
+
+
+active_voice_channels = {}  # {channel_id: message_id}
 
 @bot.event
 async def on_voice_state_update(
@@ -117,7 +185,7 @@ async def on_voice_state_update(
                 # start notification
                 date = get_date_str()
                 message = await text_channel.send(
-                    f"{date}\n🎧 <@{member.id}> が **<#{voice_channel.id}>** でボイスチャットを開始！"
+                    f'{date}\n🎧 <@{member.id}> が **<#{voice_channel.id}>** でボイスチャットを開始！'
                 )
                 active_voice_channels[voice_channel.id] = message.id
         
@@ -137,12 +205,15 @@ async def on_voice_state_update(
                     silent=True
                 )
                 active_voice_channels.pop(voice_channel.id)
+        
+        target_role_id = ROLE_ID_TEST if DEBUG else ROLE_ID_RISE
+        has_target_role = any(role.id == target_role_id for role in member.roles)
+        if has_target_role:
+            update_join_record(member.id)
 
     except Exception as e:
         await send_error_log(e, inspect.currentframe().f_code.co_name)
 
-
-JST = timezone(timedelta(hours=+9), 'JST')
 
 def get_date_str() -> str:
     now = datetime.now(JST)
@@ -153,24 +224,26 @@ def get_date_str() -> str:
 async def on_member_join(member: discord.Member):
     """
     add the role to a newly joined member
+    register member.id to join_record
     """
-    if DEBUG:
-        role_id = ROLE_ID_TEST
-        # channel_id = CHANNEL_ID_TEST_TX
-    else:
-        role_id = ROLE_ID_RISE
-        # channel_id = CHANNEL_ID_MANAGE
-
+    role_id = ROLE_ID_TEST if DEBUG else ROLE_ID_RISE
     role = member.guild.get_role(role_id)
-    # channel = bot.get_channel(channel_id)
-    
+
     if role:
         try:
             await member.add_roles(role, reason='bot自動登録')
-            # await channel.send(f'{member.mention} さんを {role.mention} に設定しました！')
-
         except Exception as e:
             await send_error_log(e, inspect.currentframe().f_code.co_name)
+
+    update_join_record(member.id, date_null=True)
+
+
+@bot.event
+async def on_member_remove(member: discord.Member):
+    """
+    remove member.id from join_record
+    """
+    remove_join_record(member.id)
 
 
 @bot.event
@@ -203,6 +276,37 @@ async def send_error_log(e: Exception, func_name: str = None):
     channel = bot.get_channel(CHANNEL_ID_TEST_TX)
     message =f'Function:{func_name}\nStacktrace:\n```{(repr(e))}```'
     await channel.send(message)
+
+
+@bot.event
+async def on_ready():
+    """
+    bot start-up
+    """
+    print(f'Logged in as {bot.user}')
+    await tree.sync()
+
+    scheduler.start()
+    
+    if not JOIN_RECORD.exists():
+        print('creating join_record...')
+
+        guild = bot.get_guild(SERVER_ID)
+        role = discord.utils.get(guild.roles, name="RISE")
+        role_members = [member for member in guild.members if role in member.roles]
+        
+        join_record = {}
+        for member in role_members:
+            join_record[str(member.id)] = None
+
+        with open(JOIN_RECORD, 'w', encoding='utf-8') as f:
+            json.dump(join_record, f)
+        print(f'created join_record as : {join_record}')
+
+    global record_start_at
+    record_start_at = datetime.now(JST).strftime('%Y/%m/%d')
+    print(f'join_record_start_at: {record_start_at}')
+
 
 
 if __name__ == '__main__':
